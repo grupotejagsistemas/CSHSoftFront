ee4254d0dc16d150647fc76b96df8cae2c402071
tomiherrero <tomas.herrero@hotmail.es>
2020-10-20 21:41:34 -0300

    add components historial

---
+++ src/app/historiales/form-historial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-historial',
+  templateUrl: './form-historial.component.html',
+  styleUrls: ['./form-historial.component.css']
+})
+export class FormHistorialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
