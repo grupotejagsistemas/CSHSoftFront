7451d92323fcd13c9ae94f9e11eb8d3852ac887a
tomiherrero <tomas.herrero@hotmail.es>
2020-11-12 21:58:17 -0300

    mascota anda

--- src/app/mascotas/editar-mascotas.component.ts
+++ src/app/mascotas/editar-mascotas.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Mascota } from './mascota';
-import {EstadoMascota } from './estadoMascota';
-import { MascotaService } from './mascota.service';
+import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import swal from 'sweetalert2';
 import Swal from 'sweetalert2';
-import { FormArray, FormBuilder, Validators } from '@angular/forms';
-
 import { AuditoriaService } from '../auditoria/auditoria.service';
 import { AuthService } from '../usuarios/auth.service';
+import { EstadoMascota } from './estadoMascota';
+import { Mascota } from './mascota';
+import { MascotaService } from './mascota.service';
 
 @Component({
   selector: 'app-editar-mascotas',
@@ -17,23 +15,20 @@ import { AuthService } from '../usuarios/auth.service';
 })
 export class EditarMascotasComponent implements OnInit {
 
-  
   mascota: Mascota;
 
   estados: EstadoMascota[];
+
   private fotoSeleccionada: File;
 
   constructor(
-    
     private mascotaService: MascotaService,
     private router: Router,
     private route: ActivatedRoute,
-    private formBuilder: FormBuilder,
     private auditoriaService: AuditoriaService,
-    private authService: AuthService
-    ) { 
-    
-  }
+    private authService: AuthService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
     const id = +this.route.snapshot.paramMap.get('id');
@@ -41,15 +36,14 @@ export class EditarMascotasComponent implements OnInit {
     if(id !== 0){
       this.mascotaService.getMascota(id).subscribe((resp: any) => {
         console.log('mascota', id)
-        this.mascotaObj = resp;
+        this.mascota = resp;
       })
     }
 
     this.mascotaService.getEstados().subscribe((resp: any) => {
-        this.estados = resp;
-
-    })
-  }
+      this.estados = resp;
+  })
+}
 
   get nombreNoValido(){
     return this.mascotaObj.get('nombre').invalid && this.mascotaObj.get('nombre').touched
@@ -79,6 +73,7 @@ export class EditarMascotasComponent implements OnInit {
     return this.mascotaObj.get('estado').invalid && this.mascotaObj.get('estado').touched
   }
   
+
   mascotaObj = this.formBuilder.group({
     nombre : ["",Validators.required],
     fechaNacimiento : ["",Validators.required],
@@ -92,78 +87,60 @@ export class EditarMascotasComponent implements OnInit {
     idEstado: [null],
     estado: [null,Validators.required]
   });
-  
-  submit(): void{
-    const id = +this.route.snapshot.paramMap.get('id');
-  
-    console.log(this.mascotaObj);  
-     if (this.mascotaObj.invalid)
-     return  Object.values(this.mascotaObj.controls).forEach(control => {
-        control.markAsTouched();
-      })
 
-      this.mascotaService.getMascota(id).subscribe((resp => {
-        this.mascota = resp;
-      }))
-    }
-  
-  
   auditoriaModificarObj = {
     usuario: this.authService.usuario.username,
     accion: 'Modificación de mascotas'
   }
-  
-  
+
   auditoriaModificar(){
     this.auditoriaService.crearAuditoria(this.auditoriaModificarObj).subscribe(response => {
       return response;
     })
   }
 
+  public submit(): void{
+    const id = +this.route.snapshot.paramMap.get('id');
+    console.log('ID', id);
+    console.log('mascota', this.mascotaObj);
+     this.mascotaService.modificarMascota(this.mascotaObj.value, id)
+     .subscribe(
+       response => {
+         this.router.navigate(['/mascotas'])
+         Swal.fire({
+           icon: 'success',
+           title: 'La mascota ha sido modificada',
+           showConfirmButton: false,
+           timer: 1500
+         })
+         if(id !== 0){
+           this.subirFoto(id.toString())
+         }
+         this.auditoriaModificar();
+         return response;
+       }
+     ) 
+   }
 
-    public modificar(): void{
-       const id = +this.route.snapshot.paramMap.get('id');
-  
-        this.mascotaService.modificarMascota(this.mascotaObj.value, id)
-        .subscribe(
-          response => {
-            this.router.navigate(['/mascotas'])
-            swal.fire({
-              icon: 'success',
-              title: 'La mascota ha sido modificada',
-              showConfirmButton: false,
-              timer: 1500
-            })
-            if(id !== 0){
-              this.subirFoto(id.toString())
-            }
-            this.auditoriaModificar();
-            return response;
-          }
-        ) 
-      }
-  
-      seleccionarFoto(event){
-        this.fotoSeleccionada = event.target.files[0];
-        console.log(this.fotoSeleccionada);
-        if(this.fotoSeleccionada.type.indexOf('image') < 0){
-          Swal.fire('Error', 'El archivo debe ser del tipo imagen', 'error');
-          this.fotoSeleccionada = null;
-        } 
-      }
-    
-      subirFoto(id: string){
-  
-        if(!this.fotoSeleccionada){
-          Swal.fire('warning', 'La mascota fue creada sin imagen', 'warning') ;
-        }else {
-          console.log('id', id);
-          this.mascotaService.subirFoto(this.fotoSeleccionada, id)
-          .subscribe(mascota => {
-            this.mascota = mascota;
-          })
-        }
-      }
-  
+   seleccionarFoto(event){
+    this.fotoSeleccionada = event.target.files[0];
+    console.log(this.fotoSeleccionada);
+    if(this.fotoSeleccionada.type.indexOf('image') < 0){
+      Swal.fire('Error', 'El archivo debe ser del tipo imagen', 'error');
+      this.fotoSeleccionada = null;
+    } 
   }
-  
+
+  subirFoto(id: string){
+
+    if(!this.fotoSeleccionada){
+      Swal.fire('warning', 'La mascota fue creada sin imagen', 'warning') ;
+    }else {
+      console.log('id', id);
+      this.mascotaService.subirFoto(this.fotoSeleccionada, id)
+      .subscribe(mascota => {
+        this.mascota = mascota;
+      })
+    }
+  }
+}
